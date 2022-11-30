4b4948438df3e085e184e18e525716cd06d694f8
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-11-30 15:05:26 +0100

    verifica della risposta del server

--- js/main.js
+++ js/main.js
@@ -5,4 +5,16 @@ createApp({
         title: "Vue Email List",
     };
   },
+    methods: {
+    fetchData() {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then(function (resp) {
+                console.log(resp);
+                console.log("L'email generata dal server è: " + resp.data.response);
+            });
+        },
+    },
+    mounted () {
+        this.fetchData()
+    }
 }).mount("#app")
