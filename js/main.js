753dd631db3b3fdcdb976f80a6a0c6004fe821d3
Pelliccia96 <fra.pelliccia96@gmail.com>
2022-11-30 15:11:47 +0100

    Visualizzazione risposta server nell'html

--- js/main.js
+++ js/main.js
@@ -3,14 +3,16 @@ createApp({
   data () {
     return {
         title: "Vue Email List",
+        listaMail: [],
     };
   },
     methods: {
     fetchData() {
         axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then(function (resp) {
+            .then((resp) => {
                 console.log(resp);
                 console.log("L'email generata dal server è: " + resp.data.response);
+                this.listaMail.push(resp.data.response);
             });
         },
     },
